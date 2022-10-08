3c56144d49cb83ab5adef2c6aca125b5d282f30e
Hugo <duprez.hugo@gmail.com>
2022-10-08 18:24:12 +0200

    feat: client side computing

--- src/code.ts
+++ src/code.ts
@@ -1,45 +1,85 @@
 import type BinaryNode from "./interfaces/BinaryNode"
 import type PredictionResult from "./interfaces/PredictionResult";
 
+import isDebugMode from "src/utils/debugMode";
 
 //Disable infinite recursivity in nodes or limit it
 const selectOnlyTopLevelNodes: boolean = true;
 const maxSubNodes: number = 3;
+const pluginUiHeight = isDebugMode ? 424 : 216;
 
 
-figma.showUI(__html__);
+figma.showUI(__html__, {height: pluginUiHeight});
 
 
 figma.ui.onmessage = async msg => {
 
   if (msg.type === "clickPredictButton") {
-    const imagesInBytes: BinaryNode[] = await renderElementsFromSelection(figma.currentPage.selection);
-    sendImagesToBackend(imagesInBytes);
+
+    if (figma.currentPage.selection.length < 1) {
+      figma.ui.postMessage({type : "emptySelection"});
+    } else {
+      const imagesInBytes: BinaryNode[] = await renderElementsFromSelection(figma.currentPage.selection);
+      sendImagesToUi(imagesInBytes);
+    }
     
   }
 
-  if (msg.type === "close") {
-    figma.closePlugin();
-  }
-
   if (msg.type === "response") {
-    const nodesToRename: SceneNode[] = selectAllNodesFromSelection(figma.currentPage.selection, "TEXT");
+
+    const nodesToRename: SceneNode[] = selectAllNodesFromSelection(figma.currentPage.selection, ["TEXT", "VECTOR"]);
     const msgPayload: PredictionResult[] = msg.payload;
     
     const startTime:  number = new Date().getTime();
 
     for (const node of nodesToRename) {
-      for (let predictionResult of msgPayload) { //figma.findNodeById(id) existe aussi
+      node.name = "Frame"; //Rename all node "Frame" before renaming to avoid conflicts in exceptions
+      for (let predictionResult of msgPayload) { //figma.findNodeById(id) existe too
         if (node.id === predictionResult.nodeId) {
           node.name = predictionResult.prediction;
         }
+      }
+
+
+      /*
+      **********
+      EXCEPTIONS HANDLING
+      Must correct node.name & parent.name references here if classes names changes in the model
+      **********
+      */
+
+      //Handle image node naming
+      if (node.type === "RECTANGLE") {
+        const fills: readonly Paint[] | typeof figma.mixed = node.fills;
+        if (fills[0].type === "IMAGE") {
+          node.name = "Image"
+        }
+      }
+      //Handle text only container naming
+      if (node.type === "FRAME" || node.type === "GROUP") {
+        const children: SceneNode[] = node.findAll();
+        if (children.length > 1) {
+          const areChildrenAllTextNodes: boolean = children.every((node) => node.type === "TEXT");
+          if (areChildrenAllTextNodes) {
+            node.name = "Paragraph container";
+          }
+        }
+      }
+      //Handle container naming
+      const parent: BaseNode & ChildrenMixin = node.parent;
+      if (parent.name === node.name && parent.name !== "Container" && parent.name !== "Card" && parent.name !== "Horizontal container" && parent.name !== "Vertical container") {
+        parent.name = `${node.name} container`;
       }
     }
 
     const endTime: number = new Date().getTime();
 
-    console.log(`FIGMA RENAME LAYERS Execution time in Figma: ${endTime - startTime}s`);
+    console.log(`[Figma]: Renaming layer time: ${endTime - startTime}s`);
 
+    //figma.closePlugin();
+  }
+
+  if (msg.type === "close") {
     figma.closePlugin();
   }
   
@@ -48,7 +88,7 @@ figma.ui.onmessage = async msg => {
 
 async function renderElementsFromSelection (selection: readonly SceneNode[]) {
 
-  const allSelectedNodes: SceneNode[] | readonly SceneNode[] = selectOnlyTopLevelNodes ? selectOnlyTopLevelNode(figma.currentPage.selection, "TEXT") : selectAllNodesFromSelection(figma.currentPage.selection, "TEXT");
+  const allSelectedNodes: SceneNode[] | readonly SceneNode[] = selectOnlyTopLevelNodes ? selectOnlyTopLevelNode(figma.currentPage.selection) : selectAllNodesFromSelection(figma.currentPage.selection, ["TEXT", "VECTOR"]);
   const binaryNodes: BinaryNode[] = await sceneNodeToBinaryNode(allSelectedNodes);
 
   return binaryNodes;
@@ -60,15 +100,53 @@ async function sceneNodeToBinaryNode (sceneNodes: SceneNode[] | readonly SceneNo
   let renderedNodes: BinaryNode[] = [];
 
   for (const node of sceneNodes) {
+
+    const baseNodeWidth: number = node.width;
+    const baseNodeHeight: number = node.height;
+    const largestMeasure = Math.max(baseNodeHeight, baseNodeWidth);
+    const ratio = Number(224 / largestMeasure).toFixed(2);
+    const nodeToRender = largestMeasure > 224 ? minifyNode(node, parseFloat(ratio)) : node;
+    const frameTheNodeToRender: SceneNode = frameANode(nodeToRender);
+
     const id: string = node.id;
-    const bytes: Uint8Array = await node.exportAsync({format : "JPG"});
+    //const bytes: Uint8Array = await node.exportAsync({format : "JPG"});
+    const bytes: Uint8Array = await frameTheNodeToRender.exportAsync({format : "JPG"});
     renderedNodes = [...renderedNodes, {nodeId : id, imageDataBytes : bytes}];
+    if (nodeToRender !== node) {
+      nodeToRender.remove();
+    }
+    if (frameTheNodeToRender !== node) {
+      frameTheNodeToRender.remove();
+    }
   }
 
   return renderedNodes;
 }
 
-function selectAllNodesFromSelection (selection: readonly SceneNode[], exludeType: string): SceneNode[] {
+function minifyNode(node: SceneNode, ratio: number): SceneNode {
+    const minifiedNode: SceneNode = node.clone();
+    minifiedNode.rescale(ratio);
+    return minifiedNode;
+}
+
+function frameANode(node: SceneNode): SceneNode {
+  const frame: FrameNode = figma.createFrame();
+  const child: SceneNode = node.clone();
+  frame.layoutMode = "HORIZONTAL";
+  frame.counterAxisAlignItems = "CENTER";
+  frame.primaryAxisAlignItems = "CENTER";
+  frame.counterAxisSizingMode = "FIXED";
+  frame.primaryAxisSizingMode = "FIXED";
+  child.layoutAlign = "INHERIT"
+  child.layoutGrow = 0;
+  frame.insertChild(0, child);
+  frame.resize(224, 224);
+  
+  
+  return frame;
+}
+
+function selectAllNodesFromSelection (selection: readonly SceneNode[], excludeTypes: string[]): SceneNode[] {
 
   let selectedNodes: SceneNode[] = [];
   let childrenFromSelectedNodes = [];
@@ -85,16 +163,22 @@ function selectAllNodesFromSelection (selection: readonly SceneNode[], exludeTyp
   const selectedNodesAndAllChildren: SceneNode[] = selectedNodes.concat(mergedChildrenFromSelectednodes);
   const selectedNodesAndAllChildrenWithoutDuplicate: SceneNode[] = [...new Set(selectedNodesAndAllChildren)]; //Remove all duplicate (TODO: improve this)
 
-  const nodesWithoutText: SceneNode[] = selectedNodesAndAllChildrenWithoutDuplicate.filter((node) => node.type !== exludeType);
+  const nodesWithoutExcluded: SceneNode[] = selectedNodesAndAllChildrenWithoutDuplicate.filter((node) => {
+    if (excludeTypes.includes(node.type)) {
+      return false;
+    } else {
+      return true;
+    }
+  });
 
-  return nodesWithoutText;
+  return nodesWithoutExcluded;
 }
 
-function selectOnlyTopLevelNode(selection: readonly SceneNode[], exludeType: string): readonly SceneNode[] {
+function selectOnlyTopLevelNode(selection: readonly SceneNode[]): readonly SceneNode[] {
   let selectedNodes: readonly SceneNode[] = selection;
   return selectedNodes;
 }
 
-function sendImagesToBackend (imagesInBytes: BinaryNode[]) {
-  figma.ui.postMessage({type : "networkRequest", data : imagesInBytes}); //Send message to browser API
+function sendImagesToUi (imagesInBytes: BinaryNode[]) {
+  figma.ui.postMessage({type : "processingRequest", data : imagesInBytes}); //Send message to browser API
 }
--- src/ui.svelte
+++ src/ui.svelte
@@ -1,79 +1,222 @@
 <script lang="ts">
-
   import "./app.css";
   import magicWand from "./lib//assets/magicWand.svg";
   import loadingCircle from "./lib/assets/loadingCircle.svg";
-  
+
+  import { onMount } from "svelte";
+
+  import type BinaryNodeJson from "./interfaces/BinaryNodeJson";
+  import type PredictionResult from "./interfaces/PredictionResult";
+  import type BinaryNode from "./interfaces/BinaryNode";
+
+  import isDebugMode from "src/utils/debugMode";
+
   let isLoading: boolean = false;
+  let emptySelection: boolean = false;
   let responseStatus: number;
+  let isOnline: boolean;
+  let isModelReady: boolean = false;
+  let sampleImage: HTMLImageElement = new Image();
+  let precision: number = 0.45;
+
+  //TM setup
+  const URL = "https://teachablemachine.withgoogle.com/models/7TY9ihr-l/";
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+  let model;
+
+  onMount( async () => {
+    isOnline = checkInternetConnection();
+    await init();
+  });
 
   const handleClick = () => {
+    isLoading = true;
     parent.postMessage({ pluginMessage: { type: "clickPredictButton" } }, "*");
   };
 
   //Handle the demand from the sandbox API to make a network request when the order is recieved
   window.onmessage = async (event) => {
-    if (event.data.pluginMessage.type === "networkRequest") {
-      try {
-        //const url = "https://figma-autoname-backend.herokuapp.com/api/predictNode" //Heroku
-        const url = "https://figma-autoname-backend-dy9w4.ondigitalocean.app/api/predictNode"; //DigitalOcean
-        //const url = "http://localhost:4001/api/predictNode"; //Localhost
-        const initObject = {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-            "x-api-key": "theBrownFox"
-          },
-          body: JSON.stringify({ imagesData: event.data.pluginMessage.data }),
-        };
-        isLoading = true;
-        const response = await fetch(url, initObject);
-        responseStatus = await response.status
-        const responseJson = await response.json();
-        window.parent.postMessage({pluginMessage : {type : "response", payload : responseJson}}, "*"); //Close plugin only when the request end
-        isLoading = false;
-      } catch (error) {
-        console.log(error.message);
-        isLoading = false;
+
+    if (event.data.pluginMessage.type === "emptySelection") {
+      emptySelection = true;
+    }
+
+    if (event.data.pluginMessage.type === "processingRequest") {
+
+      emptySelection = false;
+
+      const binaryNodes: BinaryNode[] = event.data.pluginMessage.data;
+
+      if (isDebugMode) {
+        sampleImage = await renderUint8ArrayToImage(binaryNodes[0].imageDataBytes);
       }
+
+      let results: PredictionResult[] = [];
+
+      //TM PREDICTION LOOP
+      for (let node of binaryNodes) {
+        const predictedNode: PredictionResult = await predict(node);
+        results = [...results, predictedNode];
+      }
+
+      if (isDebugMode) {
+        console.log(`[Svelte]: prediction results:`);
+        console.log(results);
+      }
+
+      //Send result to Figma sandbox
+      window.parent.postMessage({pluginMessage : {type : "response", payload : results}}, "*");
+
+      isLoading = false;
+    }
+  };
+
+  async function init() {
+    //@ts-ignore
+    model = await tmImage.load(modelURL, metadataURL);
+    isModelReady = true;
+    if (isDebugMode) {
+      console.log(`[Svelte]: Model ready`);
     }
   }
 
-  function closePlugin(): void {
-    window.parent.postMessage({ pluginMessage: { type: "close" } }, "*")
+  async function predict(node: BinaryNode): Promise<PredictionResult> {
+
+    const pixelImage: HTMLImageElement = await renderUint8ArrayToImage(node.imageDataBytes);
+
+    if (isDebugMode) {
+      sampleImage = pixelImage;
+    }
+
+    const prediction: any[] = await model.predict(pixelImage);
+    
+    let sortedProbabilities = prediction.sort((a, b) => a.probability - b.probability);
+
+    if (isDebugMode) {
+      console.log(sortedProbabilities);
+    }
+
+    let finalist = sortedProbabilities[sortedProbabilities.length - 1];
+
+    let predictedNode: PredictionResult;
+
+    if (finalist.probability > precision) {
+      predictedNode = {
+        nodeId : node.nodeId,
+        prediction : finalist.className
+      }
+    } else {
+      predictedNode = {
+        nodeId : node.nodeId,
+        prediction : "Container"
+      }
+    }
+
+    pixelImage.remove();
+
+    return predictedNode;
   }
+
+  function closePlugin(): void {
+    window.parent.postMessage({ pluginMessage: { type: "close" } }, "*");
+  }
+
+  function checkInternetConnection(): boolean {
+    let isOnline: boolean;
+    isOnline = navigator.onLine ? true : false;
+    if (isDebugMode) {
+      console.log(`[Svelte]: ${isOnline ? "Online" : "Offline"}`);
+    }
+    return isOnline;
+  }
+
+  async function renderUint8ArrayToImage(bytes: Uint8Array): Promise<HTMLImageElement> {
+    const newImage = new Image(224, 224);
+    const base64Data = btoa(String.fromCharCode.apply(null, bytes)); //No Buffer.from(bytes).toString('base64'); cause we are not in Node JS
+    newImage.src = "data:image/png;base64," + base64Data;
+    return newImage;
+  }
+
 
 </script>
 
+<svelte:head />
 
+<main
+  class="flex flex-col items-center justify-between px-4 py-4 h-full bg-[#2C2C2C]"
+>
+  <title-container class="flex flex-col items-center w-full space-y-4">
+    <h1 class="text-base font-medium text-white text-center mt-2">
+      {isModelReady ? `Select layers and press "Name"` : `Please wait the model loading`}
+    </h1>
 
-<main class="flex flex-col items-center justify-between px-4 py-4 h-full bg-Black">
+    {#if isDebugMode}
+      <p
+        class="text-gray-400 text-xs px-2 py-1 border-[1px] w-fit border-gray-400 rounded"
+      >
+        Debug mode
+      </p>
+      <p class="text-gray-400 justify-center text-xs">
+        Image sent to the model 👇
+      </p>
+      <img
+        src={sampleImage.src}
+        alt="Pixels sent to the model"
+        bind:this={sampleImage}
+        class="rounded-md"
+      />
+    {/if}
+  </title-container>
 
-  <h1 class="text-base font-medium text-white text-center mt-2">Select layers and press "Name"</h1>
+  <body-container class="flex flex-col w-full items-center">
+    {#if responseStatus === 401}
+      <p class="text-xs text-white font-medium mb-4">Non authorized :/</p>
+    {:else if !isDebugMode}
+      <magic-wand-container class="p-4 rounded-full flex flex-col items-center justify-center m-6">
+        <img src={magicWand} alt="Magic wand icon" class="-translate-x-[3px] translate-y-[2px]"/>
+      </magic-wand-container>
+    {/if}
 
-  {#if responseStatus === 401}
-    <p class="text-xs text-white font-medium">Non authorized :/</p>
-  {:else if responseStatus < 250}
-  <p class="text-xs text-white font-medium">Error {responseStatus}</p>
-  {:else}
-    <img src={magicWand} alt="Magic wand icon">
-  {/if}
-
-  {#if responseStatus > 250}
-    <button
-      class="w-full flex flex-row justify-center items-center bg-Blue px-3 py-[7px] text-xs text-white font-medium rounded-md"
-      on:click={closePlugin}>
-      Close
-    </button>
-  {:else}
-    <button
-      class="w-full flex flex-row justify-center items-center bg-Blue px-3 py-[7px] text-xs text-white font-medium rounded-md"
-      on:click={handleClick}>
-      {#if isLoading}
-        <img src={loadingCircle} alt="Loading circle" class="animate-spin mr-2">
-      {/if}
-      {isLoading ? `Processing...` : `Name`}
-    </button>
-  {/if}
-
+    {#if  !isModelReady}
+      <button
+      class="w-full flex flex-row justify-center items-center bg-Blue px-3 py-[7px] text-xs cursor-not-allowed grayscale text-white font-medium rounded-md"
+      >
+        <img
+          src={loadingCircle}
+          alt="Loading circle"
+          class="animate-spin mr-2"
+        />
+        Loading model...
+      </button>
+    {:else if !isOnline}
+      <button
+        class="w-full flex flex-row justify-center items-center bg-Blue px-3 py-[7px] text-xs cursor-not-allowed grayscale text-white font-medium rounded-md"
+      >
+        No connection :/
+      </button>
+    {:else if emptySelection}
+      <button
+        class="w-full flex flex-row justify-center items-center bg-Blue px-3 py-[7px] text-xs text-white font-medium rounded-md"
+        on:click={handleClick}
+      >
+        Please selects layers
+      </button>
+    {:else}
+      <button
+        class="w-full flex flex-row justify-center items-center bg-Blue px-3 py-[7px] text-xs text-white font-medium rounded-md"
+        class:cursor-not-allowed={isLoading}
+        on:click={handleClick}
+      >
+        {#if isLoading}
+          <img
+            src={loadingCircle}
+            alt="Loading circle"
+            class="animate-spin mr-2"
+          />
+        {/if}
+        {isLoading ? `Processing...` : `Name`}
+      </button>
+    {/if}
+  </body-container>
 </main>
