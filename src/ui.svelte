0127fb4e94086e2973ff71ad77faeddfd9a422c1
JooHyung Park <dusskapark@gmail.com>
2023-08-16 13:35:27 +0800

    Update ui.svelte

--- src/ui.svelte
+++ src/ui.svelte
@@ -590,14 +590,14 @@
     <body-container class="flex flex-col w-full h-full items-center">
       {#if selectionInDevMode}
         <div
-          class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+          class="w-full max-w-sm border rounded-lg bg-gray-800 border-gray-700"
         >
         <h5
-        class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white ml-4 mt-4"
+        class="text-xl font-semibold tracking-tight text-white ml-4 mt-4"
       >
         Autoname Prediction
       </h5>
-          <div class="flex rounded-lg border flex-col items-center m-4">
+          <div class="flex rounded-lg border flex-col items-center m-4 bg-white">
             {#if sampleImage}
               <img
                 src={sampleImage.src}
@@ -608,15 +608,15 @@
             {/if}
           </div>
           <div class="px-5 pb-5">
-            <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+            <ul class="max-w-md space-y-1 list-inside text-gray-400">
               <li class="flex items-center">
-                <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <svg class="w-3.5 h-3.5 mr-2 text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                </svg>
                {selectionInDevMode.prediction}
               </li>
               <li class="flex items-center">
-                <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <svg class="w-3.5 h-3.5 mr-2 text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                </svg>
                {selectionInDevMode.probability}
